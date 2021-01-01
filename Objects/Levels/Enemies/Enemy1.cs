0705c534c49decc2bf75334f1d2b982d3c77570f
Peter S <peter@anyline.io>
2021-01-02 00:39:59 +0100

    something something turret

--- Objects/Levels/Enemies/Enemy1.cs
+++ Objects/Levels/Enemies/Enemy1.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using Wyri.Main;
 using Wyri.Types;
+using Wyri.Util;
 
 namespace Wyri.Objects.Levels.Enemies
 {
@@ -14,15 +15,20 @@ namespace Wyri.Objects.Levels.Enemies
         int maxShootTimeout;
 
         float angle = 0;
-        Vector2 target;
         (bool, float) rayCast;
         Vector2 offvec = new Vector2(0, -4);
         float crossHairTimeout, maxCrossHairTimeout;
+
+        Vector2 target;
+        Vector2 newTarget;
+        float distance = 144;
+        float lineAlpha = 0;
 
         enum State
         {
             Idle,
             Target,
+            PrepareShoot,
             Shoot
         }
 
@@ -34,8 +40,6 @@ namespace Wyri.Objects.Levels.Enemies
             maxCrossHairTimeout = 30;
             crossHairTimeout = maxCrossHairTimeout;
             shootTimeout = maxShootTimeout;
-
-
             s = State.Idle;
         }
 
@@ -44,92 +48,95 @@ namespace Wyri.Objects.Levels.Enemies
             base.Update();
 
             var ang = M.VectorToAngle(MainGame.Player.Center - Center + offvec);
+            var rc = this.RayCast(MainGame.Player, ang, 1, distance);
 
             switch (s)
             {
                 case State.Idle:
+                    shootTimeout = maxShootTimeout;
                     crossHairTimeout = maxCrossHairTimeout;
-                    var rc = this.RayCast(MainGame.Player, ang, 1, 80);
                     if (rc.Item1)
-                    {
+                    {                        
                         rayCast = rc;
                         angle = ang;
                         s = State.Target;
                     }
                     break;
                 case State.Target:
-                    shootTimeout = Math.Max(shootTimeout - 1, 0);
-                    rc = this.RayCast(MainGame.Player, ang, 1, 80);
                     if (!rc.Item1)
                     {
                         rayCast = rc;
-
                         shootTimeout = Math.Min(shootTimeout + 3, maxShootTimeout);
                         if (shootTimeout == maxShootTimeout)
                             s = State.Idle;
                     }
                     else
                     {
+                        lineAlpha = Math.Min(lineAlpha + .1f, 1);
+                        shootTimeout = Math.Max(shootTimeout - 1, 0);
                         rayCast = rc;
                         angle = ang;
                     }
                     if (shootTimeout == 0)
                     {
+                        s = State.PrepareShoot;
+                    }
+                    break;
+                case State.PrepareShoot:
+                    angle = ang;
+                    crossHairTimeout = Math.Max(crossHairTimeout - 1, 0);
+                    if (crossHairTimeout == 0)
+                    {                        
                         s = State.Shoot;
                     }
                     break;
                 case State.Shoot:
-                    crossHairTimeout = Math.Max(crossHairTimeout - 1, 0);
-                    if (crossHairTimeout == 0)
-                    {                        
-                        shootTimeout = maxShootTimeout;
-                        s = State.Idle;
+                    {
+                        lineAlpha = Math.Max(lineAlpha - .1f, 0);
                     }
                     break;
                 default:
                     break;
             }
 
-            if (rayCast.Item1)
-                target = Center + new Vector2(M.LengthDirX(angle) * rayCast.Item2, M.LengthDirY(angle) * rayCast.Item2);
+            target = Center + new Vector2(M.LengthDirX(angle) * rayCast.Item2, M.LengthDirY(angle) * rayCast.Item2);
 
-            //angle = M.VectorToAngle(MainGame.Player.Center - Center);
-            //rayCast = this.RayCast(MainGame.Player, angle, 1, 80);
+            var rndx = RND.Next * (float)shootTimeout / (float)maxShootTimeout * 8;
+            var rndy = RND.Next * (float)shootTimeout / (float)maxShootTimeout * 8;
 
-            //if (rayCast.Item1)
-            //{
-            //    shootTimeout = Math.Max(shootTimeout - 1, 0);
-            //}
-            //else
-            //{
-            //    shootTimeout = Math.Min(shootTimeout + 3, maxShootTimeout);
-            //}
+            newTarget = target + new Vector2(rndx, rndy);
+
         }
 
         public override void Draw(SpriteBatch sb)
         {
             sb.Draw(GameResources.Enemy1[0], Position, null, Color.White, 0, new Vector2(8), Vector2.One, SpriteEffects.None, G.D_ENEMY);
-            if (shootTimeout < maxShootTimeout)
-            {
-                var rs = 1f - (float)shootTimeout / (float)maxShootTimeout;
 
-                float r = 255;
-                float g = 255 - rs * 255;
-                float b = 255 - rs * 255;
-                float a = rs * 255;
+            var rs = 1f - (float)shootTimeout / (float)maxShootTimeout;
 
-                var color = new Color((byte)r, (byte)g, (byte)b, (byte)a);
-                var hairColor = (crossHairTimeout < maxCrossHairTimeout) ? Color.White : new Color(color, 1.0f);
+            float r = 255;
+            float g = 255 - rs * 255;
+            float b = 255 - rs * 255;
+            float a = rs * 255;
 
-                var off = 1 + (float)crossHairTimeout / (float)maxCrossHairTimeout * 3;
+            var color = new Color((int)r, (int)g, (int)b);
+            color = new Color(color, lineAlpha);
+            var hairColor = Color.White;
 
-                sb.DrawLine(Center, target, color, G.D_EFFECT);
-                sb.Draw(GameResources.Crosshair[0], target - new Vector2(4) + new Vector2(-off, -off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
-                sb.Draw(GameResources.Crosshair[1], target - new Vector2(4) + new Vector2(off, -off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
-                sb.Draw(GameResources.Crosshair[2], target - new Vector2(4) + new Vector2(-off, off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
-                sb.Draw(GameResources.Crosshair[3], target - new Vector2(4) + new Vector2(off, off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
+            if (s != State.Idle)
+            {                
+                
+                sb.DrawLine(Center, newTarget, color, G.D_EFFECT);
+
+                if (s != State.Target)
+                {
+                    var off = -4 + (float)crossHairTimeout / (float)maxCrossHairTimeout * 8;
+                    sb.Draw(GameResources.Crosshair[0], newTarget - new Vector2(4) + new Vector2(-off, -off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
+                    sb.Draw(GameResources.Crosshair[1], newTarget - new Vector2(4) + new Vector2(off, -off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
+                    sb.Draw(GameResources.Crosshair[2], newTarget - new Vector2(4) + new Vector2(-off, off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
+                    sb.Draw(GameResources.Crosshair[3], newTarget - new Vector2(4) + new Vector2(off, off), null, hairColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, G.D_EFFECT);
+                }
             }
-
 
             base.Draw(sb);
         }
