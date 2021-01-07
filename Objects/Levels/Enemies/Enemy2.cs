e8cacc4648dc2a7ba73c81c855aa2e8f1b4d1af1
Peter S <peter@anyline.io>
2021-01-07 22:03:32 +0100

    enemy 2 working

--- Objects/Levels/Enemies/Enemy2.cs
+++ Objects/Levels/Enemies/Enemy2.cs
@@ -44,11 +44,23 @@ namespace Wyri.Objects.Levels.Enemies
 
             waitTimer = Math.Max(waitTimer - 1, 0);
 
-            var rc = this.RayCast(MainGame.Player, (direction == Direction.Left) ? 180 : 0, 1, 48);
-
-            if (rc.Item1)
+            var seenPlayer = this.RayCast(MainGame.Player, M.VectorToAngle(MainGame.Player.Center - Position), 1, 80).Item1;
+            if (seenPlayer)
             {
-                state = State.Run;
+                if ((direction == Direction.Left && MainGame.Player.X < X) ||
+                    (direction == Direction.Right && MainGame.Player.X > X))
+                {
+                    if (MainGame.Player.Bottom >= Top - 16 && MainGame.Player.Top <= Bottom + 8)
+                    {
+                        if (state == State.Walk)
+                            state = State.Run;
+                    }
+                }
+            }
+            else
+            {
+                if (state == State.Run)
+                    state = State.Walk;
             }
 
             if (state == State.Idle)
@@ -86,25 +98,43 @@ namespace Wyri.Objects.Levels.Enemies
             }
             else if (state == State.Run)
             {
+                if (seenPlayer)
+                {
+                    if (X > MainGame.Player.X + 16)
+                    {
+                        //if (direction == Direction.Right)
+                        //    xVel = 0;
+                        direction = Direction.Left;
+                    }
+                    if (X < MainGame.Player.X - 16)
+                    {
+                        //if (direction == Direction.Left)
+                        //    xVel = 0;
+                        direction = Direction.Right;
+                    }
+                }
+
                 animAmpl = 2;
                 waitTimer = 2 * 60;
                 maxAnimSpd = 3;
                 animSpd = maxAnimSpd;
-                xVel = Math.Sign((int)direction) * Math.Min(Math.Abs(xVel) + .05f, 1.25f);
+                if (direction == Direction.Left)
+                    xVel = Math.Max(xVel - .05f, -1.5f);
+                if (direction == Direction.Right)
+                    xVel = Math.Min(xVel + .05f, 1.5f);
             }
 
             s = animSpd / maxAnimSpd;
 
-            X += xVel;
-
             var groundTile = this.CollisionSolidTile((int)direction * 16, BBox.h, true);
-            if (!this.CollisionSolidTile(xVel + Math.Sign(xVel) * 16, 0) && groundTile)
-            {                
-                
+            if (!this.CollisionSolidTile(xVel + Math.Sign(xVel) * 4, 0) && groundTile)
+            {
+                X += xVel;
             }
             else
             {
-                xVel = Math.Sign((int)direction) * Math.Max(Math.Abs(xVel) - .12f, 0);
+                //xVel = Math.Sign((int)direction) * Math.Max(Math.Abs(xVel) - .175f, 0);
+                xVel = 0;
 
                 if (Math.Abs(xVel) == 0)
                 {
@@ -129,7 +159,7 @@ namespace Wyri.Objects.Levels.Enemies
             var i = (int)direction;
 
             var headOffset = new Vector2(0, M.Sin((t + 1) % (float)(2 * Math.PI)) * .3f + 2);
-            var legOffset = new Vector2(0, state == State.Idle ? Math.Min(M.Sin((t) % (float)(2 * Math.PI)) * .5f, 0) : -1f);
+            var legOffset = new Vector2(0, state == State.Idle ? Math.Min(M.Sin((t) % (float)(2 * Math.PI)) * .5f, .5f) : -1f);
 
             var body = -(t + .5f) % (float)(2 * Math.PI);
             var head = -(t) % (float)(2 * Math.PI);
@@ -138,16 +168,16 @@ namespace Wyri.Objects.Levels.Enemies
             var leg3 = (t + 3 * i) % (float)(2 * Math.PI);
 
             var leg4 = (t + 4 * i) % (float)(2 * Math.PI);
-            var leg5 = (t + 5 * i) % (float)(2 * Math.PI);
-            var leg6 = (t + 6 * i) % (float)(2 * Math.PI);
+            var leg5 = (t + 6 * i) % (float)(2 * Math.PI);
+            var leg6 = (t + 8 * i) % (float)(2 * Math.PI);
 
             var bodyAngle = M.DegToRad(-1 + M.Sin(body) * 2) * i;
             var leg1Angle = M.DegToRad(-1 + M.Sin(leg1) * 4) * i;
-            var leg2Angle = M.DegToRad(-3 + M.Sin(leg2) * 6) * i;
+            var leg2Angle = M.DegToRad(-2 + M.Sin(leg2) * 4) * i;
             var leg3Angle = M.DegToRad(-1 + M.Sin(leg3) * 4) * i;
             var leg4Angle = M.DegToRad(-1 + M.Sin(leg4) * 4) * i;
-            var leg5Angle = M.DegToRad(-3 + M.Sin(leg5) * 6) * i;
-            var leg6Angle = M.DegToRad(-1 + M.Sin(leg6) * 4) * i;
+            var leg5Angle = M.DegToRad(-1 + M.Sin(leg5) * 2) * i;
+            var leg6Angle = M.DegToRad(2 + M.Sin(leg6) * 2) * i;
 
             float dBody = G.D_ENEMY;
             float dHead = G.D_ENEMY - .00001f;
@@ -168,7 +198,7 @@ namespace Wyri.Objects.Levels.Enemies
             
             var leg4Pos = new Vector2(i * 7, 1) + new Vector2(M.Sin(leg4) * -i * .2f, M.Cos(leg4) * 1.5f) * s + legOffset;
             var leg5Pos = new Vector2(i * 1, 1) + new Vector2(M.Sin(leg5) * -i * .2f, M.Cos(leg5) * 1.5f) * s + legOffset;
-            var leg6Pos = new Vector2(i * -1, 0) + new Vector2(M.Sin(leg6) * -i * .2f, M.Cos(leg6) * 1.5f) * s + legOffset;
+            var leg6Pos = new Vector2(i * -4, 0) + new Vector2(M.Sin(leg6) * -i * .2f, M.Cos(leg6) * 1.5f) * s + legOffset;
 
             leg1Pos = new Vector2(leg1Pos.X, Math.Min(leg1Pos.Y, .5f));
             leg2Pos = new Vector2(leg2Pos.X, Math.Min(leg2Pos.Y, .5f));
@@ -178,7 +208,7 @@ namespace Wyri.Objects.Levels.Enemies
             leg6Pos = new Vector2(leg6Pos.X, Math.Min(leg6Pos.Y, .5f));
 
             sb.Draw(GameResources.Enemy2[3], Position + leg1Pos, null, Color.White, leg1Angle, new Vector2(8), new Vector2(i, 1), SpriteEffects.None, dLegFront);
-            sb.Draw(GameResources.Enemy2[3], Position + leg2Pos, null, Color.White, leg2Angle, new Vector2(8), new Vector2(i, 1), SpriteEffects.None, dLegFront + .00001f);
+            sb.Draw(GameResources.Enemy2[7], Position + leg2Pos, null, Color.White, leg2Angle, new Vector2(8), new Vector2(i, 1), SpriteEffects.None, dLegFront + .00001f);
             sb.Draw(GameResources.Enemy2[4], Position + leg3Pos, null, Color.White, leg3Angle, new Vector2(8), new Vector2(i, 1), SpriteEffects.None, dLegFront);
 
             sb.Draw(GameResources.Enemy2[5], Position + leg4Pos, null, Color.White, leg4Angle, new Vector2(8), new Vector2(i, 1), SpriteEffects.None, dLegBack);
